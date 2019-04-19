86bf77cf08d0600852ef70c16c3039e02d3497ad
daid <daid303@gmail.com>
2019-04-19 21:06:53 +0200

    Have snipe towers aim at both players

--- resources/ai/snipe.lua
+++ resources/ai/snipe.lua
@@ -1,6 +1,22 @@
-function update()
-    local angle = (getPlayer(0).getPosition() - self.getPosition()):angle()
+function getAngleDiffTowardsPlayer(index)
+    local player = getPlayer(index)
+    if player == nil or not player.valid then
+        return nil
+    end
+    local angle = (player.getPosition() - self.getPosition()):angle()
     local diff = angle-self.getTurretRotation()
+    if diff > 180 then diff = diff - 360 end
+    if diff < -180 then diff = diff + 360 end
+    return diff
+end
+
+function update()
+    local diff0 = getAngleDiffTowardsPlayer(0)
+    local diff1 = getAngleDiffTowardsPlayer(1)
+    local diff = diff0
+    if diff == nil or math.abs(diff) > math.abs(diff1) then diff = diff1 end
+    if diff == nil then return end
+    
     if diff > 180 then diff = diff - 360 end
     if diff < -180 then diff = diff + 360 end
     if diff > 5 then
